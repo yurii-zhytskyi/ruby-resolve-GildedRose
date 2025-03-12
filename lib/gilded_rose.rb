c00d43d8105c853a61f57c0abefbfa0e7f83b7f7
Yurii Zhytskyi <yuriizhytskyi@gmail.com>
2025-03-12 15:08:51 +0200

    002-specs Added GildedRose spec.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,4 +1,4 @@
-
+require 'item'
 
 class GildedRose
 
@@ -52,19 +52,5 @@ class GildedRose
         end
       end
     end
-  end
-end
-
-class Item
-  attr_accessor :name, :sell_in, :quality
-
-  def initialize(name, sell_in, quality)
-    @name = name
-    @sell_in = sell_in
-    @quality = quality
-  end
-
-  def to_s()
-    "#{@name}, #{@sell_in}, #{@quality}"
   end
 end
---
+++ spec/gilded_rose_spec.rb
@@ -0,0 +1,145 @@
+require 'rspec'
+
+require 'gilded_rose'
+
+describe GildedRose do
+  subject(:glided_rose) { described_class.new([item]) }
+
+  let(:quality) { 20 }
+  let(:sell_in) { 10 }
+  let(:name) { 'foo' }
+  let(:item) { Item.new(name, sell_in, quality) }
+
+  describe '#update_quality' do
+    before do
+      glided_rose.update_quality
+    end
+
+    shared_examples 'item.name' do
+      it 'does not change the name' do
+        expect(item.name).to eq name
+      end
+    end
+
+    shared_examples 'item' do
+      it_behaves_like 'item.name'
+
+      it 'reduces sell_in by one' do
+        expect(item.sell_in).to eq sell_in - 1
+      end
+    end
+
+    context 'with normal item' do
+      it_behaves_like 'item'
+
+      it 'reduces quality by one' do
+        expect(item.quality).to eq quality - 1
+      end
+
+      context 'when quality is zero' do
+        let(:quality) { 0 }
+
+        it 'does not change quality to negative' do
+          expect(item.quality).to eq 0
+        end
+      end
+
+      context 'when sell_in is negative' do
+        let(:sell_in) { -1 }
+
+        it 'reduces quality by two (twice as fast)' do
+          expect(item.quality).to eq quality - 2
+        end
+      end
+    end
+
+    context 'with "Aged Brie" item' do
+      let(:name) { 'Aged Brie' }
+
+      it_behaves_like 'item'
+
+      it 'increments quality by one' do
+        expect(item.quality).to eq quality + 1
+      end
+
+      context 'when quality is fifty' do
+        let(:quality) { 50 }
+
+        it 'does not increment quality more than 50' do
+          expect(item.quality).to eq 50
+        end
+      end
+    end
+
+    context 'with "Sulfuras" item' do
+      let(:name) { 'Sulfuras, Hand of Ragnaros' }
+      let(:quality) { 80 }
+
+      it_behaves_like 'item.name'
+
+      it 'does not change sell_in' do
+        expect(item.sell_in).to eq sell_in
+      end
+
+      it 'has constant quality' do
+        expect(item.quality).to eq 80
+      end
+    end
+
+    context 'with "Backstage passes" item' do
+      let(:name) { 'Backstage passes to a TAFKAL80ETC concert' }
+
+      it_behaves_like 'item'
+
+      context 'when sell_in is more than 10 days' do
+        let(:sell_in) { 11 }
+
+        it 'increases quality by one' do
+          expect(item.quality).to eq quality + 1
+        end
+      end
+
+      context 'when sell_in is between 6 and 10 days' do
+        let(:sell_in) { 10 }
+
+        it 'increases quality by two' do
+          expect(item.quality).to eq quality + 2
+        end
+      end
+
+      context 'when sell_in is between 0 and 5 days' do
+        let(:sell_in) { 5 }
+
+        it 'increases quality by three' do
+          expect(item.quality).to eq quality + 3
+        end
+      end
+
+      context 'when sell_in is less than 0 days' do
+        let(:sell_in) { -1 }
+
+        it 'set quality to zero' do
+          expect(item.quality).to eq 0
+        end
+      end
+    end
+
+    context 'with "Conjured" item' do
+      let(:name) { 'Conjured Mana Cake' }
+
+      it_behaves_like 'item'
+
+      it 'reduces quality by two (twice as fast as normal items)' do
+        expect(item.quality).to eq quality - 2
+      end
+
+      context 'when sell_in is negative' do
+        let(:sell_in) { -1 }
+
+        it 'reduces quality by four (twice as fast and twice as fast as normal items)' do
+          expect(item.quality).to eq quality - 4
+        end
+      end
+    end
+  end
+end
